9fda7021253345f5331cd26298cf5f510195a481
Johan <johan@johanswork.com>
2023-04-06 16:03:39 +0200

    memory_track::allocator_base to simplify making new tracking allocators

--- water/memory_track/allocator.hpp
+++ water/memory_track/allocator.hpp
@@ -4,113 +4,52 @@
 //\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_/\_
 #ifndef WATER_MEMORY_TRACK_ALLOCATOR_HPP
 #define WATER_MEMORY_TRACK_ALLOCATOR_HPP
-#include <water/memory_track/bits.hpp>
+#include <water/memory_track/allocator_base.hpp>
 namespace water { namespace memory_track {
 
 /*
 
 Allocates from memory_track::memory
-Keeps track of the allocation function and optionally an "address", free must be made with the matching function + "address"
 
-The address can be useful for testing when something should use the same allocator-object for allocate + free
+The constructor needs a memory_track::memory object or it cannot allocate memory.
+
+Look at allocator_base.hpp.
 
 */
 
 template<typename memory_, typename exception_ = void>
-class allocator
+class allocator :
+    public allocator_base<allocator<memory_, exception_>, memory_, exception_>
 {
-public:
-    using memory_type = memory_;
-    using tag_type = typename memory_type::tag_type;
-    using exception = exception_;
-    static bool constexpr is_noexcept = types::equal_plain<void, exception>::result;
-
-private:
-    memory_type *mymemory = 0;
-    tag_type mytag {};
-    void const* myaddress = 0; // keep this 0
+    using base_ = allocator_base<allocator<memory_, exception_>, memory_, exception_>;
+    
+    memory_ *mymemory = 0;
 
 public:
-    allocator() = default;
 
-    allocator(memory_type& memory, tag_type const& tag = {}) :
-        mymemory(&memory),
-        mytag(tag)
+    constexpr allocator() = default;
+
+    constexpr allocator(memory_& memory, typename memory_::tag_type const& tag = {}) :
+        base_{tag},
+        mymemory{&memory}
     {}
     
-    bool operator==(allocator const& a) const noexcept {
-        return mymemory == a.mymemory && mytag == a.mytag;
+    memory_* memory_pointer() noexcept {
+        return mymemory;
     }
     
-    bool operator!=(allocator const& a) const noexcept {
-        return !(*this == a);
+    memory_ const* memory_pointer() const noexcept {
+        return mymemory;
     }
-
-    tag_type const& tag() const noexcept {
-        return mytag;
-    }
-
-    allocator& tag(tag_type const& a) noexcept {
-        mytag = a;
-        return *this;
-    }
-
-    void const* address() const noexcept {
-        return myaddress;
-    }
-
-    allocator& address(void const* a) noexcept {
-        // when freeing, this address must match what it was when memory was allocated. default is 0.
-        myaddress = a;
-        return *this;
-    }
-
-    void* allocate(size_t bytes) noexcept(is_noexcept) {
-        return allocate_do(bytes, tag_function<void>);
-    }
-
-    void free(void *pointer, size_t bytes) noexcept {
-        free_do(pointer, bytes, tag_function<void>);
-    }
-
-    template<typename type_>
-    type_* allocate() noexcept(is_noexcept) {
-        return static_cast<type_*>(allocate_do(sizeof(type_), tag_function<type_>));
-    }
-
-    template<typename type_>
-    void free(void *pointer) noexcept {
-        free_do(pointer, sizeof(type_), tag_function<type_>);
-    }
-
-    template<typename type_>
-    type_* allocate(size_t count) noexcept(is_noexcept) {
-        return static_cast<type_*>(allocate_do(sizeof(type_) * count, tag_function<type_[1]>));
-    }
-
-    template<typename type_>
-    void free(void *pointer, size_t count) noexcept {
-        free_do(pointer, sizeof(type_) * count, tag_function<type_[1]>);
-    }
-
-private:
-    void* allocate_do(size_t bytes, void (*tag)()) noexcept(is_noexcept) {
-        auto pointer = mymemory->allocate(bytes, mytag, allocator_tag(myaddress, tag));
-        if(!pointer) throw_if<exception>();
-        return pointer;
-    }
-
-    void free_do(void *pointer, size_t bytes, void (*tag)()) noexcept {
-        mymemory->free(pointer, bytes, mytag, allocator_tag(myaddress, tag));
-    }
-
-    template<typename type_>
-    static void tag_function()
-    {}
+    
+    using base_::operator==;
+    using base_::operator!=;
 };
+
 
 template<typename memory_> using allocator_throw = allocator<memory_, exception>;
 template<typename memory_> using allocator_nothrow = allocator<memory_>;
+
 
 }}
 #endif
